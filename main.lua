b7093b2562a35949c25f99452fc7405d03cafc54
Euller Peixoto <peixotoeuller500@gmail.com>
2022-01-30 05:45:11 -0200

    update script

--- main.lua
+++ main.lua
@@ -1,3 +1,34 @@
-function love.draw()
-    love.graphics.print("Hello world", 325, 300)
+function love.load()
+    x = 100
+    y = 100
+    fruits = {"apple", "banana"}
+
+    for i = 1, #fruits do
+        fruit = fruits[i]
+        if i == 2 then
+            i = 1
+        end
+    end
+
 end
+
+function love.update(dt)
+    if love.keyboard.isDown("right") then
+        x = x + 100 * dt
+    elseif love.keyboard.isDown("left") then
+        x = x - 100 * dt
+    elseif love.keyboard.isDown("up") then
+        y = y - 100 * dt
+    elseif love.keyboard.isDown("down") then
+        y = y + 100 * dt
+    end
+end
+
+function love.draw()
+    for i, fruit in ipairs(fruits) do
+        love.graphics.print(fruit, 100, 100 + 50 * i)
+    end
+
+    love.graphics.rectangle("line", x, y, 200, 150)
+end
+
